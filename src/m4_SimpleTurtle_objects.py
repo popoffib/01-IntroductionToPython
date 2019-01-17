5c239e6b78a4fdf3e28623c96037713492979977
popoffib <popoffib@rose-hulman.edu>
2019-01-17 16:49:14 -0500

    done

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -167,7 +167,7 @@ izzy.backward(250)
 
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
