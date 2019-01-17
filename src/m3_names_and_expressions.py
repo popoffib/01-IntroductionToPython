ba27fb76fc44d63ad602b67187ce7948af84acd5
popoffib <popoffib@rose-hulman.edu>
2019-01-17 15:46:05 -0500

    finished types

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -55,7 +55,7 @@ print(math.sqrt(2))
 print(2**0.5)
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number").
@@ -72,24 +72,36 @@ print(2**0.5)
 #   will print     <class 'float'>
 #   Try it now!
 #   (Just write   print(type(3.14))   below this _TODO_ and run the program.)
-#
+print(type(3.14))
 #   Now go through the BLAH objects listed below, one by one.  For each:
 #      1. Try to GUESS its TYPE.
 #      2. Then write code of the form   print(type(BLAH)).
 #      3. THen RUN the code to LEARN its TYPE.
 
 #       "hello"
+print(type("hello"))
 #       'hello'
+print(type('hello'))
 #       'a b c'
+print(type('a b c'))
 #       3 + 3
+print(type(3 +3))
 #       "3" + "3"
+print(type("3" + "3"))
 #       2 ** 100
+print(type(2 ** 100))
 #       2.0 ** 100
+print(type(2.0 ** 100))
 #       math.sin(8)
+print(type(math.sin(8)))
 #       math.sin
+print(type(math.sin))
 #       print
+print(type(print))
 #       math
+print(type(math))
 #       'math'
+print(type('math'))
 #
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
